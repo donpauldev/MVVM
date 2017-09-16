65f283460a3fab8adeeb50e91008a54784fa2dc9
Don Paul <donpaulpm92@gmail.com>
2017-09-16 04:37:45 +0000

    Update MVVMViewController.swift

--- MVVMProject/MVVMViewController.swift
+++ MVVMProject/MVVMViewController.swift
@@ -20,10 +20,10 @@ class MVVMViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        loadViews()
+        setupView()
     }
     
-    func loadViews() {
+    func setupView() {
         name.attributedText = dogViewModel.name
         breed.text = dogViewModel.breed
         color.text = dogViewModel.color
